ce21457c4bc755167d5a37b35af25c9f9bf40273
Sahil Meghani <sahilmeghani5467@gmail.com>
2026-03-08 23:32:46 +0530

    Improve login page ui

--- surveysnap/core/forms.py
+++ surveysnap/core/forms.py
@@ -19,5 +19,14 @@ class UserSignupForm(UserCreationForm):
         }
     
 class UserLoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "class": "block w-full pl-11 pr-4 h-14 border border-slate-200 bg-white rounded-lg focus:ring-2 focus:ring-primary/20 focus:border-primary outline-none",
+        })
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "block w-full pl-11 pr-12 h-14 border border-slate-200 bg-white rounded-lg focus:ring-2 focus:ring-primary/20 focus:border-primary outline-none",
+        })
+    )
