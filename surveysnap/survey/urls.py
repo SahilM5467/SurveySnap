c8255912aa7dfabff12f7da2199f08aaa7797acd
Sahil Meghani <sahilmeghani5467@gmail.com>
2026-03-15 22:42:01 +0530

    create templates of admin, survey creator and respondent

--- surveysnap/survey/urls.py
+++ surveysnap/survey/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin/",views.AdminDashboardView,name="admin_dashboard"),
-    path("creator/",views.creatorDashboardView,name="creator_dashboard"),
-    path("respondent/",views.respondentDashboardView,name="respondent_dashboard"),
+    path("admin/admin_dashboard",views.AdminDashboardView,name="admin_dashboard"),
+    path("admin/manage_users",views.MangeUsersView,name="manage_users"),
+    path("admin/manage_surveys",views.MangeSurveysView,name="manage_surveys"),
+    path("admin/manage_templates",views.ManageTemplatesView,name="manage_templates"),
+    path("admin/reports",views.ReportsView,name="reports"),
+
+    path("creator/creator_dashboard",views.CreatorDashboardView,name="creator_dashboard"),
+    path("creator/create_survey",views.CreateSurveyView,name="create_survey"),
+    path("creator/my_surveys",views.MySurveysView,name="my_surveys"),
+    path("creator/analytics",views.AnalyticsView,name="analytics"),
+    
+    path("respondent/respondent_dashboard",views.RespondentDashboardView,name="respondent_dashboard"),
+    path("respondent/available_surveys",views.AvailableSurveysView,name="available_surveys"),
+    path("respondent/my_responses",views.MyResponsesView,name="my_responses"),
+    path("respondent/profile",views.ProfileView,name="profile"),
 ]
--- surveysnap/survey/views.py
+++ surveysnap/survey/views.py
@@ -1,20 +1,70 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .decorators import role_required
+from core.models import User
+from survey.models import Survey, Response
 
 # Create your views here.
+
+
+# Admin :
 
 # @login_required(login_url="login") 
 @role_required(allowed_roles=["admin"])
 def AdminDashboardView(request):
     return render(request,"survey/admin/admin_dashboard.html")
+    
+@role_required(allowed_roles=["admin"])
+def MangeUsersView(request):
+    return render(request,"survey/admin/manage_users.html")
+
+@role_required(allowed_roles=["admin"])
+def MangeSurveysView(request):
+    return render(request,"survey/admin/manage_surveys.html")
+
+@role_required(allowed_roles=["admin"])
+def ManageTemplatesView(request):
+    return render(request,"survey/admin/manage_templates.html")
+
+@role_required(allowed_roles=["admin"])
+def ReportsView(request):
+    return render(request,"survey/admin/reports.html")
+
+
+# Survey Creator :
 
 # @login_required(login_url="login")
 @role_required(allowed_roles=["creator"])
-def creatorDashboardView(request):
+def CreatorDashboardView(request):
     return render(request,"survey/creator/creator_dashboard.html")
+
+@role_required(allowed_roles=["creator"])
+def CreateSurveyView(request):
+    return render(request,"survey/creator/create_survey.html")
+
+@role_required(allowed_roles=["creator"])
+def MySurveysView(request):
+    return render(request,"survey/creator/my_surveys.html")
+
+@role_required(allowed_roles=["creator"])
+def AnalyticsView(request):
+    return render(request,"survey/creator/analytics.html")
+
+# Respodent :
 
 # @login_required(login_url="login")
 @role_required(allowed_roles=["respondent"])
-def respondentDashboardView(request):
+def RespondentDashboardView(request):
     return render(request,"survey/respondent/respondent_dashboard.html")
+
+@role_required(allowed_roles=["respondent"])
+def AvailableSurveysView(request):
+    return render(request,"survey/respondent/available_surveys.html")
+
+@role_required(allowed_roles=["respondent"])
+def MyResponsesView(request):
+    return render(request,"survey/respondent/my_responses.html")
+
+@role_required(allowed_roles=["respondent"])
+def ProfileView(request):
+    return render(request,"survey/respondent/profile.html")
